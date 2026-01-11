665b88d93b1a7929adb4fb4b4418a309296c5a5c
ea234 <anyang@gmx.de>
2026-01-11 15:45:26 +0100

    Day 23 - Part 1 - List of all Connections sorted

--- src/de/ea234/aoc2024/day23/Day23LanParty.java
+++ src/de/ea234/aoc2024/day23/Day23LanParty.java
@@ -74,7 +74,15 @@ public class Day23LanParty
     wl( getConnectedToComputer( "tc" ) );
     wl( getConnectedToComputer( "td" ) );
     wl( "" );
-    wl( getSortedListInputNew() );
+    //wl( getSortedListInputNew() );
+
+    List< String > list_all_connections = getListAllConnections();
+    
+    for ( String cur_connection : list_all_connections )
+    {
+      wl( cur_connection );
+    }
+
 
     /*
      * 
@@ -100,6 +108,30 @@ public class Day23LanParty
     * td,wh,yn
     *
 
+    Wenn A folgende Verbindungen hat
+     
+       A - B
+       A - C
+       A - D
+       
+    dann ist der Ring geschlossen, wenn es 
+    
+       B - C
+       C - B
+       
+    gibt. Ist das nicht der Fall, wird die 
+    Verbindung nach D geprueft: 
+    
+      A - B
+      A - D
+    
+   Es muss dann eine Verbindung 
+   
+      B - D
+      D - B 
+      
+   geben.
+
 
  Finde direkte Verbindungen.
  Finde eine Verbindung derart: <computer a> nach <computer b>, <computer b> nach <computer c>, <computer c> nach <computer a>
@@ -121,8 +153,6 @@ public class Day23LanParty
      
      Eingabe Liste der Verbindungen die es zu testen gilt
      
-     
-
 aq-cg
     cg-aq - fällt weg, shon enthalten
 
@@ -315,6 +345,109 @@ aq-cg
      * 
      */
   }
+
+
+  public static String checkPair( String pConnection1, String pConnection2 )
+  {
+    String[] connected_computers_1 = pConnection1.split( "-" );
+
+    String from_1 = null;
+    String to_1 = null;
+
+    String from_2 = null;
+    String to_2 = null;
+
+    if ( connected_computers_1[ 0 ].compareToIgnoreCase( connected_computers_1[ 1 ] ) < 0 )
+    {
+      wl( "A " + connected_computers_1[ 0 ] + " + " + connected_computers_1[ 1 ] );
+
+      from_1 = connected_computers_1[ 0 ];
+      to_1 = connected_computers_1[ 1 ];
+    }
+    else
+    {
+      wl( "B " + connected_computers_1[ 1 ] + " + " + connected_computers_1[ 0 ] );
+
+      from_1 = connected_computers_1[ 1 ];
+      to_1 = connected_computers_1[ 0 ];
+    }
+
+    String[] connected_computers_2 = pConnection1.split( "-" );
+
+    if ( connected_computers_2[ 0 ].compareToIgnoreCase( connected_computers_2[ 1 ] ) < 0 )
+    {
+      wl( "A " + connected_computers_2[ 0 ] + " + " + connected_computers_2[ 1 ] );
+
+      from_2 = connected_computers_1[ 0 ];
+      to_2 = connected_computers_1[ 1 ];
+
+    }
+    else
+    {
+      wl( "B " + connected_computers_2[ 1 ] + " + " + connected_computers_2[ 0 ] );
+
+      from_2 = connected_computers_1[ 1 ];
+      to_2 = connected_computers_1[ 0 ];
+    }
+
+    if ( from_1.equals( from_2 ) )
+    {
+      String check_connection = to_1 + "-" + to_2;
+
+      boolean verbindung_existiert = false;
+
+      if ( verbindung_existiert )
+      {
+        return check_connection;
+      }
+
+      check_connection = to_2 + "-" + to_1;
+
+      verbindung_existiert = false;
+
+      if ( verbindung_existiert )
+      {
+        return check_connection;
+      }
+    }
+
+    return null;
+  }
+
+  private static List< String > getListAllConnections()
+  {
+    List< String > list_all_connections = new ArrayList< String >();
+
+    List< String > keys_all_computer_names = new ArrayList< String >( m_hash_map.keySet() );
+
+    Collections.sort( keys_all_computer_names );
+
+    for ( String key_computer_name_from : keys_all_computer_names )
+    {
+      String[] input_parts = m_hash_map.get( key_computer_name_from ).split( "," );
+
+      List< String > list_computer_names = new ArrayList< String >();
+
+      for ( String computer_name : input_parts )
+      {
+        if ( computer_name != null && !computer_name.isEmpty() )
+        {
+          list_computer_names.add( computer_name );
+        }
+      }
+
+      Collections.sort( list_computer_names );
+
+      for ( String computer_name_to : list_computer_names )
+      {
+        list_all_connections.add( key_computer_name_from + "-" + computer_name_to );
+      }
+    }
+
+    return list_all_connections;
+  }
+
+
 
   private static HashMap< String, String > m_hash_map = null;
 
