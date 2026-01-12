c0ef5843c68fdf78f9a99411f9389788f12e35b4
ea234 <anyang@gmx.de>
2026-01-12 13:54:07 +0100

    Day 23 - Part 1 - proof of concept

--- src/de/ea234/aoc2024/day23/Day23LanParty.java
+++ src/de/ea234/aoc2024/day23/Day23LanParty.java
@@ -28,6 +28,7 @@ public class Day23LanParty
     String test_1 = "kh-tc,qp-kh,de-cg,ka-co,yn-aq,qp-ub,cg-tb,vc-aq,tb-ka,wh-tc,yn-cg,kh-ub,ta-co,de-co,tc-td,tb-wq,wh-td,ta-ka,td-qp,aq-cg,wq-ub,ub-vc,de-ta,wq-aq,wq-vc,wh-yn,ka-de,kh-ta,co-tc,wh-qp,tb-vc,td-yn";
 
     calculatePart01( test_1, true );
+
   }
 
   private static void calculatePart01( String pString, boolean pKnzDebug )
@@ -74,389 +75,282 @@ public class Day23LanParty
     wl( getConnectedToComputer( "tc" ) );
     wl( getConnectedToComputer( "td" ) );
     wl( "" );
-    //wl( getSortedListInputNew() );
 
     List< String > list_all_connections = getListAllConnections();
-    
+
     for ( String cur_connection : list_all_connections )
     {
       wl( cur_connection );
     }
 
+    /*
+     *
+     * aq,cg,yn
+     * aq,vc,wq
+     * co,de,ka
+     * co,de,ta
+     * co,ka,ta
+     * de,ka,ta
+     * kh,qp,ub
+     * qp,td,wh
+     * tb,vc,wq
+     * tc,td,wh
+     * td,wh,yn
+     * ub,vc,wq
+     *     
+     * co,de,ta
+     * co,ka,ta
+     * de,ka,ta
+     * qp,td,wh
+     * tb,vc,wq
+     * tc,td,wh
+     * td,wh,yn
+     *
+     * 
+     *  0  Computer    aq - ,cg,vc,wq,yn
+     *  1  Computer    cg - ,aq,de,tb,yn
+     *  2  Computer    co - ,de,ka,ta,tc
+     *  3  Computer    de - ,cg,co,ka,ta
+     *  4  Computer    ka - ,co,de,ta,tb
+     *  5  Computer    kh - ,qp,ta,tc,ub
+     *  6  Computer    qp - ,kh,td,ub,wh
+     *  7  Computer    ta - ,co,de,ka,kh
+     *  8  Computer    tb - ,cg,ka,vc,wq
+     *  9  Computer    tc - ,co,kh,td,wh
+     * 10  Computer    td - ,qp,tc,wh,yn
+     * 11  Computer    ub - ,kh,qp,vc,wq
+     * 12  Computer    vc - ,aq,tb,ub,wq
+     * 13  Computer    wh - ,qp,tc,td,yn
+     * 14  Computer    wq - ,aq,tb,ub,vc
+     * 15  Computer    yn - ,aq,cg,td,wh
+     * 
+     * 
+     * ----------------------------------------------
+     * Computers connected to "ta"
+     * 
+     * 1  Computer    de - ,cg,co,ka,ta * co,ka,ta
+     * 2  Computer    co - ,de,ka,ta,tc * de,ka,ta
+     * 3  Computer    ka - ,co,de,ta,tb * co,de,ta
+     * 
+     * 4  Computer    kh - ,qp,ta,tc,ub 
+     * 
+     * ----------------------------------------------
+     * Computers connected to "tb"
+     * 
+     * 1  Computer    cg - ,aq,de,tb,yn
+     * 2  Computer    vc - ,aq,tb,ub,wq
+     * 3  Computer    ka - ,co,de,ta,tb
+     * 4  Computer    wq - ,aq,tb,ub,vc
+     * 
+     * ----------------------------------------------
+     * Computers connected to "tc"
+     * 
+     * 1  Computer    co - ,de,ka,ta,tc
+     * 2  Computer    td - ,qp,tc,wh,yn
+     * 3  Computer    wh - ,qp,tc,td,yn
+     * 4  Computer    kh - ,qp,ta,tc,ub
+     * 
+     * ----------------------------------------------
+     * Computers connected to "td"
+     * 
+     * 1  Computer    tc - ,co,kh,td,wh
+     * 2  Computer    wh - ,qp,tc,td,yn
+     * 3  Computer    yn - ,aq,cg,td,wh
+     * 4  Computer    qp - ,kh,td,ub,wh
+    
+    */
 
     /*
      * 
-    * aq,cg,yn
-    * aq,vc,wq
-    * co,de,ka
-    * co,de,ta
-    * co,ka,ta
-    * de,ka,ta
-    * kh,qp,ub
-    * qp,td,wh
-    * tb,vc,wq
-    * tc,td,wh
-    * td,wh,yn
-    * ub,vc,wq
-    *     
-    * co,de,ta
-    * co,ka,ta
-    * de,ka,ta
-    * qp,td,wh
-    * tb,vc,wq
-    * tc,td,wh
-    * td,wh,yn
-    *
-
-    Wenn A folgende Verbindungen hat
-     
-       A - B
-       A - C
-       A - D
-       
-    dann ist der Ring geschlossen, wenn es 
-    
-       B - C
-       C - B
-       
-    gibt. Ist das nicht der Fall, wird die 
-    Verbindung nach D geprueft: 
-    
-      A - B
-      A - D
-    
-   Es muss dann eine Verbindung 
-   
-      B - D
-      D - B 
-      
-   geben.
-
-
- Finde direkte Verbindungen.
- Finde eine Verbindung derart: <computer a> nach <computer b>, <computer b> nach <computer c>, <computer c> nach <computer a>
-
- Also, finde nicht alle Verbindungen zu einem Computer, sondern 
- finde in der Eingabe einen Verbindungsring, bei welchem 3 Computer enthalten sind und der letzte 
- Computer gleich wieder der Anfangscomputer ist.
-
- cq = cg-aq,cg-de,cg-tb,cg-yn
-
-
-1. Hashmap
-     Key Computer-Name 1 
-     Key CSV-Liste der Verbindungen <computer name 1> nach <computer name 2> 
-
-2. Rekursion
-     Endbedinung = 3 Computer
-     Endbedingung = liste erschöpft
-     
-     Eingabe Liste der Verbindungen die es zu testen gilt
-     
-aq-cg
-    cg-aq - fällt weg, shon enthalten
-
-    cg-de
-        de-cg
-        de-co
-        de-ka
-        de-ta
-    
-    cg-tb
-        tb-cg
-        tb-ka
-        tb-vc
-        tb-wq
-    
-    cg-yn
-        yn-aq -------- Start Computer name
-        yn-cg
-        yn-td
-        yn-wh
-
-
-    * aq,cg,yn
-    *
-    * aq-cg
-    * 
-    * yn-aq
-    * yn-cg
-    *
-    * cg-tb
-    * 
-    * Finde direkte ring pare
-    * ag - cg   cg - yn   yn - ag 
-    * 
-    * 
-    * * aq,vc,wq
-    * vc-aq
-    * 
-    *  aq - vc  wq-aq   
-
-
-    Result Lan Party 
-    
-     0  Computer    aq - ,cg,vc,wq,yn
-     1  Computer    cg - ,aq,de,tb,yn
-     2  Computer    co - ,de,ka,ta,tc
-     3  Computer    de - ,cg,co,ka,ta
-     4  Computer    ka - ,co,de,ta,tb
-     5  Computer    kh - ,qp,ta,tc,ub
-     6  Computer    qp - ,kh,td,ub,wh
-     7  Computer    ta - ,co,de,ka,kh
-     8  Computer    tb - ,cg,ka,vc,wq
-     9  Computer    tc - ,co,kh,td,wh
-    10  Computer    td - ,qp,tc,wh,yn
-    11  Computer    ub - ,kh,qp,vc,wq
-    12  Computer    vc - ,aq,tb,ub,wq
-    13  Computer    wh - ,qp,tc,td,yn
-    14  Computer    wq - ,aq,tb,ub,vc
-    15  Computer    yn - ,aq,cg,td,wh
-    
-    ----------------------------------------------
-    Computers connected to "t"
-    
-     1  Computer    de - ,cg,co,ka,ta
-     2  Computer    cg - ,aq,de,tb,yn
-     3  Computer    co - ,de,ka,ta,tc
-     4  Computer    vc - ,aq,tb,ub,wq
-     5  Computer    tc - ,co,kh,td,wh
-     6  Computer    td - ,qp,tc,wh,yn
-     7  Computer    wh - ,qp,tc,td,yn
-     8  Computer    yn - ,aq,cg,td,wh
-     9  Computer    ka - ,co,de,ta,tb
-    10  Computer    wq - ,aq,tb,ub,vc
-    11  Computer    kh - ,qp,ta,tc,ub
-    12  Computer    qp - ,kh,td,ub,wh
-    
-    
-    ----------------------------------------------
-    Computers connected to "ta"
-    
-    1  Computer    de - ,cg,co,ka,ta
-    2  Computer    co - ,de,ka,ta,tc
-    3  Computer    ka - ,co,de,ta,tb
-    4  Computer    kh - ,qp,ta,tc,ub
-    
-    ----------------------------------------------
-    Computers connected to "tb"
-    
-    1  Computer    cg - ,aq,de,tb,yn
-    2  Computer    vc - ,aq,tb,ub,wq
-    3  Computer    ka - ,co,de,ta,tb
-    4  Computer    wq - ,aq,tb,ub,vc
-    
-    ----------------------------------------------
-    Computers connected to "tc"
-    
-    1  Computer    co - ,de,ka,ta,tc
-    2  Computer    td - ,qp,tc,wh,yn
-    3  Computer    wh - ,qp,tc,td,yn
-    4  Computer    kh - ,qp,ta,tc,ub
-    
-    ----------------------------------------------
-    Computers connected to "td"
-    
-    1  Computer    tc - ,co,kh,td,wh
-    2  Computer    wh - ,qp,tc,td,yn
-    3  Computer    yn - ,aq,cg,td,wh
-    4  Computer    qp - ,kh,td,ub,wh
-    
-    
-    aq-cg
-    aq-vc
-    aq-wq
-    aq-yn
-    
-    cg-aq
-    cg-de
-    cg-tb
-    cg-yn
-    
-    co-de
-    co-ka
-    co-ta
-    co-tc
-    
-    de-cg
-    de-co
-    de-ka
-    de-ta
-    
-    ka-co
-    ka-de
-    ka-ta
-    ka-tb
-    
-    kh-qp
-    kh-ta
-    kh-tc
-    kh-ub
-    
-    qp-kh
-    qp-td
-    qp-ub
-    qp-wh
-    
-    ta-co
-    ta-de
-    ta-ka
-    ta-kh
-    
-    tb-cg
-    tb-ka
-    tb-vc
-    tb-wq
-    
-    tc-co
-    tc-kh
-    tc-td
-    tc-wh
-    
-    td-qp
-    td-tc
-    td-wh
-    td-yn
-    
-    ub-kh
-    ub-qp
-    ub-vc
-    ub-wq
-    
-    vc-aq
-    vc-tb
-    vc-ub
-    vc-wq
-    
-    wh-qp
-    wh-tc
-    wh-td
-    wh-yn
-    
-    wq-aq
-    wq-tb
-    wq-ub
-    wq-vc
-    
-    yn-aq
-    yn-cg
-    yn-td
-    yn-wh
+     * <computer a1> to <computer b1>, <computer b1> to <computer c1>, <computer c1> to <computer a1>
      * 
+     * Computer A has the following connections
+     *  
+     *    A - B
+     *    A - C
+     *    A - D
+     *    A - Z
+     *
+     * The first two connections are checked.
+     *  
+     *    A - B
+     *    A - C
+     *
+     * If there is a circle (a group of 3) then there 
+     * have to be a connection between B and C
+     * 
+     *    B - C
+     *    C - B
+     *    
+     * If thats not the case, the first and the 
+     * second connection are checked. 
+     * 
+     *   A - B
+     *   A - D
+     * 
+     * There should be a Connection between B and D. 
+     * 
+     *   B - D
+     *   D - B 
+     *   
+     *
+     * -------------------------------------------------
+     * 
+     * yn-aq  1 with 2   1 with 3   1 with 4
+     * yn-cg  2 with 1   2 with 3   2 with 4
+     * yn-td  3 with 1   3 with 2   3 with 4
+     * yn-wh  4 with 1   4 with 3   4 with 1
+     * 
+     * yn-aq  1 with 2   1 with 3   1 with 4
+     * yn-cg             2 with 3   2 with 4
+     * yn-td                        3 with 4
+     * yn-wh                      
+     *
+     * -------------------------------------------------
+     * 
+     * n+0 = 1
+     * n+1 = 2
+     * n+2 = 3
+     * n+3 = 4
+     * 
+     * yn-aq  n+0 with n+1   n+0 with n+2   n+0 with n+3
+     * yn-cg                 n+1 with n+2   n+1 with n+3
+     * yn-td                                n+2 with n+3
+     * yn-wh                      
+     * 
+     * 
+     * n+0 with n+1    
+     * n+0 with n+2  
+     * n+0 with n+3
+     * 
+     * n+1 with n+2  
+     * n+1 with n+3
+     * 
+     * n+2 with n+3
+     * 
+     * 
+     * ------------------------------------
+     * 
+     *  index 1    0 = aq-cg = cg   
+     *  index 2    1 = aq-vc = vc   
+     * 
+     *  con_check_1      cg-vc  connection exists  false
+     *  con_check_2      vc-cg  connection exists  false
+     * 
+     * 
+     * ------------------------------------
+     * 
+     *  index 1    0 = aq-cg = cg   
+     *  index 2    2 = aq-wq = wq   
+     * 
+     *  con_check_1      cg-wq  connection exists  false
+     *  con_check_2      wq-cg  connection exists  false
+     * 
+     * 
+     * ------------------------------------
+     * 
+     *  index 1    0 = aq-cg = cg   
+     *  index 2    3 = aq-yn = yn   
+     * 
+     *  con_check_1      cg-yn  connection exists  true
+     *  con_check_2      yn-cg  connection exists  true
+     * 
+     * 
+     * con_exists     true
+     * 
+     * index_1 0
+     * index_2 3
+     * 
+     * 
+     * Loop aq - cg - yn  
      */
-  }
 
+    List< String > list_all_connections_c1 = getListAllConnectionsC( "aq" );
 
-  public static String checkPair( String pConnection1, String pConnection2 )
-  {
-    String[] connected_computers_1 = pConnection1.split( "-" );
+    int count_con = list_all_connections_c1.size();
 
-    String from_1 = null;
-    String to_1 = null;
+    boolean con_exists = false;
 
-    String from_2 = null;
-    String to_2 = null;
+    int break_1 = count_con - 1;
+    int break_2 = count_con;
 
-    if ( connected_computers_1[ 0 ].compareToIgnoreCase( connected_computers_1[ 1 ] ) < 0 )
+    int con_1_index = 0;
+    int con_2_index = 0;
+
+    while ( ( con_exists == false ) && ( con_1_index < break_1 ) )
     {
-      wl( "A " + connected_computers_1[ 0 ] + " + " + connected_computers_1[ 1 ] );
+      String con_1_connection_string = list_all_connections.get( con_1_index );
 
-      from_1 = connected_computers_1[ 0 ];
-      to_1 = connected_computers_1[ 1 ];
-    }
-    else
-    {
-      wl( "B " + connected_computers_1[ 1 ] + " + " + connected_computers_1[ 0 ] );
+      String[] con_1_array_computer_names = con_1_connection_string.split( "-" );
 
-      from_1 = connected_computers_1[ 1 ];
-      to_1 = connected_computers_1[ 0 ];
-    }
-
-    String[] connected_computers_2 = pConnection1.split( "-" );
-
-    if ( connected_computers_2[ 0 ].compareToIgnoreCase( connected_computers_2[ 1 ] ) < 0 )
-    {
-      wl( "A " + connected_computers_2[ 0 ] + " + " + connected_computers_2[ 1 ] );
-
-      from_2 = connected_computers_1[ 0 ];
-      to_2 = connected_computers_1[ 1 ];
-
-    }
-    else
-    {
-      wl( "B " + connected_computers_2[ 1 ] + " + " + connected_computers_2[ 0 ] );
-
-      from_2 = connected_computers_1[ 1 ];
-      to_2 = connected_computers_1[ 0 ];
-    }
-
-    if ( from_1.equals( from_2 ) )
-    {
-      String check_connection = to_1 + "-" + to_2;
-
-      boolean verbindung_existiert = false;
-
-      if ( verbindung_existiert )
+      for ( con_2_index = con_1_index + 1; con_2_index < break_2; con_2_index++ )
       {
-        return check_connection;
-      }
+        /*
+         * Get the connections from the list of connections 
+         */
+        String con_2_connection_string = list_all_connections.get( con_2_index );
 
-      check_connection = to_2 + "-" + to_1;
+        /*
+         * Split the connection-string to obtain the target computer names
+         */
+        String[] con_2_array_computer_names = con_2_connection_string.split( "-" );
 
-      verbindung_existiert = false;
+        /*
+         * Create the connections to be checked.
+         */
+        String con_1_connection_string_new = con_1_array_computer_names[ 1 ] + "-" + con_2_array_computer_names[ 1 ];
+        String con_2_connection_string_new = con_2_array_computer_names[ 1 ] + "-" + con_1_array_computer_names[ 1 ];
 
-      if ( verbindung_existiert )
-      {
-        return check_connection;
-      }
-    }
+        con_exists = list_all_connections.contains( con_1_connection_string_new );
 
-    return null;
-  }
-
-  private static List< String > getListAllConnections()
-  {
-    List< String > list_all_connections = new ArrayList< String >();
-
-    List< String > keys_all_computer_names = new ArrayList< String >( m_hash_map.keySet() );
-
-    Collections.sort( keys_all_computer_names );
-
-    for ( String key_computer_name_from : keys_all_computer_names )
-    {
-      String[] input_parts = m_hash_map.get( key_computer_name_from ).split( "," );
-
-      List< String > list_computer_names = new ArrayList< String >();
-
-      for ( String computer_name : input_parts )
-      {
-        if ( computer_name != null && !computer_name.isEmpty() )
+        if ( con_exists == false )
         {
-          list_computer_names.add( computer_name );
+          con_exists = list_all_connections.contains( con_2_connection_string_new );
+        }
+
+        wl( "" );
+        wl( "------------------------------------" );
+        wl( "" );
+        wl( String.format( " index 1 %4d = %s = %s   ", con_1_index, con_1_connection_string, con_1_array_computer_names[ 1 ] ) );
+        wl( String.format( " index 2 %4d = %s = %s   ", con_2_index, con_2_connection_string, con_2_array_computer_names[ 1 ] ) );
+        wl( "" );
+        wl( " connection 1 " + con_1_connection_string_new + "  connection exists  " + list_all_connections.contains( con_1_connection_string_new ) );
+        wl( " connection 2 " + con_2_connection_string_new + "  connection exists  " + list_all_connections.contains( con_2_connection_string_new ) );
+
+        if ( con_exists )
+        {
+          break;
         }
       }
 
-      Collections.sort( list_computer_names );
-
-      for ( String computer_name_to : list_computer_names )
+      if ( con_exists == false )
       {
-        list_all_connections.add( key_computer_name_from + "-" + computer_name_to );
+        con_1_index++;
       }
     }
 
-    return list_all_connections;
+    wl( "" );
+    wl( "" );
+    wl( "con_exists     " + con_exists );
+    wl( "" );
+    wl( "index_1 " + con_1_index );
+    wl( "index_2 " + con_2_index );
+    wl( "" );
+    wl( "" );
+
+    String con_1 = list_all_connections.get( con_1_index );
+    String con_2 = list_all_connections.get( con_2_index );
+
+    String[] con_1_computer = con_1.split( "-" );
+    String[] con_2_computer = con_2.split( "-" );
+
+    wl( "Loop " + con_1_computer[ 0 ] + " - " + con_1_computer[ 1 ] + " - " + con_2_computer[ 1 ] + "  " );
   }
-
-
 
   private static HashMap< String, String > m_hash_map = null;
 
   private static void addConnectedComputers( String pComputerNameA, String pComputerNameB )
   {
-    /*
-     * Assuming that both Strings are not null
-     */
-
     String list_computer = getHashMapValue( pComputerNameA );
 
     if ( list_computer == null )
@@ -542,9 +436,44 @@ aq-cg
     return "," + String.join( ",", list_computer_names );
   }
 
-  private static String getSortetListConnectedComputerX( String pComputerFrom, String pInput )
+  private static List< String > getListAllConnections()
   {
-    String[] input_parts = pInput.split( "," );
+    List< String > list_all_connections = new ArrayList< String >();
+
+    List< String > keys_all_computer_names = new ArrayList< String >( m_hash_map.keySet() );
+
+    Collections.sort( keys_all_computer_names );
+
+    for ( String key_computer_name_from : keys_all_computer_names )
+    {
+      String[] input_parts = m_hash_map.get( key_computer_name_from ).split( "," );
+
+      List< String > list_computer_names = new ArrayList< String >();
+
+      for ( String computer_name : input_parts )
+      {
+        if ( computer_name != null && !computer_name.isEmpty() )
+        {
+          list_computer_names.add( computer_name );
+        }
+      }
+
+      Collections.sort( list_computer_names );
+
+      for ( String computer_name_to : list_computer_names )
+      {
+        list_all_connections.add( key_computer_name_from + "-" + computer_name_to );
+      }
+    }
+
+    return list_all_connections;
+  }
+
+  private static List< String > getListAllConnectionsC( String pKeyComputerNameFrom )
+  {
+    List< String > list_all_connections = new ArrayList< String >();
+
+    String[] input_parts = m_hash_map.get( pKeyComputerNameFrom ).split( "," );
 
     List< String > list_computer_names = new ArrayList< String >();
 
@@ -558,31 +487,12 @@ aq-cg
 
     Collections.sort( list_computer_names );
 
-    StringBuilder sb = new StringBuilder();
-
     for ( String computer_name_to : list_computer_names )
     {
-      sb.append( pComputerFrom + "-" + computer_name_to + "\n" );
+      list_all_connections.add( pKeyComputerNameFrom + "-" + computer_name_to );
     }
 
-    return sb.toString();
-  }
-
-  private static String getSortedListInputNew()
-  {
-    String debug_string = "";
-
-    List< String > keys = new ArrayList< String >( m_hash_map.keySet() );
-
-    Collections.sort( keys );
-
-    for ( String entry_key : keys )
-    {
-      debug_string += "\n" + getSortetListConnectedComputerX( entry_key, m_hash_map.get( entry_key ) );
-    }
-
-    return debug_string;
-
+    return list_all_connections;
   }
 
   private static int getCountConnectedComputer( String pInput )
@@ -614,5 +524,4 @@ aq-cg
   {
     System.out.println( pString );
   }
-
 }
